a0121b8d7e23e5823722659d16603af02a543f2a
stephenhky <stephenhky@yahoo.com.hk>
2019-01-07 22:54:23 -0500

    empty tests

---
+++ test/test_linearregression.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+
+class LinearRegressionTester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_noregression(self):
+        pass
+
+    def test_ridge(self):
+        pass
+
+    def test_lasso(self):
+        pass
+
+    def test_both(self):
+        pass
+
+    def test_random(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
