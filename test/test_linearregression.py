b492a1643a29921e2e5f5cdd45f11a61a6bff3b2
stephenhky <stephenhky@yahoo.com.hk>
2019-01-12 23:55:06 -0500

    unit test for linear regression

--- test/test_linearregression.py
+++ test/test_linearregression.py
@@ -1,16 +1,25 @@
 
 import unittest
 
+import numpy as np
+from tfnumpy.regression.linear import fit_linear_regression
+
 
 class LinearRegressionTester(unittest.TestCase):
     def setUp(self):
-        pass
+        self.xtrain = np.array([[0.], [1.], [2.], [3.]])
+        self.ytrain = np.array([[-1.199071], [1.098184], [3.399706], [5.700929]])
 
     def tearDown(self):
         pass
 
-    def test_noregression(self):
-        pass
+    def test_regression(self):
+        regressed_results = fit_linear_regression(self.xtrain, self.ytrain, max_iter=2000)
+        print(regressed_results)
+        self.assertAlmostEqual(regressed_results['b'], -1.19, 0.01)
+        self.assertAlmostEqual(regressed_results['theta'], 2.30, 0.01)
+        self.assertEqual(regressed_results['nbfeatures'], 1)
+        self.assertEqual(regressed_results['nbtrain'], 4)
 
     def test_ridge(self):
         pass
