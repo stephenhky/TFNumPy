6758458542f854879e77abfa4cabc2e59fd7f050
stephenhky <stephenhky@yahoo.com.hk>
2019-01-13 00:11:53 -0500

    unit test for linear regression (con't)

--- test/test_linearregression.py
+++ test/test_linearregression.py
@@ -2,7 +2,7 @@
 import unittest
 
 import numpy as np
-from tfnumpy.regression.linear import fit_linear_regression
+from tfnumpy.regression import fit_linear_regression
 
 
 class LinearRegressionTester(unittest.TestCase):
@@ -15,11 +15,10 @@ class LinearRegressionTester(unittest.TestCase):
 
     def test_regression(self):
         regressed_results = fit_linear_regression(self.xtrain, self.ytrain, max_iter=2000)
-        print(regressed_results)
-        self.assertAlmostEqual(regressed_results['b'], -1.19, 0.01)
-        self.assertAlmostEqual(regressed_results['theta'], 2.30, 0.01)
         self.assertEqual(regressed_results['nbfeatures'], 1)
         self.assertEqual(regressed_results['nbtrain'], 4)
+        self.assertAlmostEqual(regressed_results['theta'][0], 2.30, 2)
+        self.assertAlmostEqual(regressed_results['b'], -1.20, 2)
 
     def test_ridge(self):
         pass
@@ -30,8 +29,6 @@ class LinearRegressionTester(unittest.TestCase):
     def test_both(self):
         pass
 
-    def test_random(self):
-        pass
 
 
 if __name__ == '__main__':
