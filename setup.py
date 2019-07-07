d4d9a541aabbde1994212af900b973004efecf37
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2019-07-07 17:00:39 -0400

    version updating

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def package_description():
 
 
 setup(name='tfnumpy',
-      version="0.0.5a0",
+      version="0.0.5a1",
       description="Collection of Simple Numerical Routines using TensorFlow",
       long_description=package_description(),
       long_description_content_type='text/markdown',
