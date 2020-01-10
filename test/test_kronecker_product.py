a42639f6dae10f6bc44561cdad01588e0931f8de
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-01-10 00:05:58 -0500

    simplified testing of Kronecker product

--- test/test_kronecker_product.py
+++ test/test_kronecker_product.py
@@ -11,22 +11,11 @@ class test_KJ(unittest.TestCase):
         self.x1 = np.array([[1, 1.4, 0], [2, 3, 3], [-1, -2, 1.3]], dtype=np.float32)
         self.x2 = np.array([[2, 3], [1, 5]], dtype=np.float32)
 
-        self.answer = np.array([[2., 3., 2.8, 4.2, 0., 0.],
-                                [1., 5., 1.4, 7., 0., 0.],
-                                [4., 6., 6., 9., 6., 9.],
-                                [2., 10., 3., 15., 3., 15.],
-                                [-2., -3., -4., -6., 2.6, 3.9],
-                                [-1., -5., -2., -10., 1.3, 6.5]], dtype=np.float32)
-
     def test_nopredefinedsession(self):
         result = kronecker_product(self.x1, self.x2)
-        self.assertTrue(np.allclose(self.answer, result, rtol=1e-5))
-
-    def test_npkron(self):
-        result = np.kron(self.x1, self.x2)
-        self.assertTrue(np.allclose(self.answer, result, rtol=1e-5))
+        self.assertTrue(np.allclose(np.kron(self.x1, self.x2), result, rtol=1e-5))
 
     def test_predefinedsession(self):
         sess = tf.Session()
         result = kronecker_product(self.x1, self.x2, tfsess=sess)
-        self.assertTrue(np.allclose(self.answer, result, rtol=1e-5))
+        self.assertTrue(np.allclose(np.kron(self.x1, self.x2), result, rtol=1e-5))
